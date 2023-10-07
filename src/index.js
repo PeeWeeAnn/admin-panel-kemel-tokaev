779f07e46829928e86033eb04c53f99c4ef45b95
Evgeny Arsaev <arsaevgeny@gmail.com>
2023-10-08 01:38:38 +0600

    feat(upload-photo-modal): add mocks, animation, preview

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./scss/app.scss"
 
+// Регистрация и авторизация VVV
+
 const loginButton = document.querySelector(".form-submit__button")
 
 const showPasswordButtons = document.querySelectorAll(
@@ -10,12 +12,14 @@ const inputs = document.querySelectorAll(
 	".form-data-email__input, .form-data-password__input, .form-data-name__input",
 )
 
-if (showPasswordButtons) {
+if (showPasswordButtons.length) {
 	for (let button of showPasswordButtons) {
 		button.addEventListener("click", (e) => {
 			e.preventDefault()
+
 			const passwordInput = e.currentTarget.previousElementSibling
 			passwordInput.focus()
+
 			if (passwordInput && passwordInput.type == "password") {
 				passwordInput.type = "text"
 				button.children[0].src = "../src/img/icons/view.svg"
@@ -27,52 +31,50 @@ if (showPasswordButtons) {
 	}
 }
 
-if (loginButton) {
-	loginButton.addEventListener("click", (e) => {
-		e.preventDefault()
-		if (inputs) {
-			for (let input of inputs) {
-				if (!input.value.length) {
-					input.style.border = "1px solid #8f1515"
+loginButton?.addEventListener("click", (e) => {
+	e.preventDefault()
+	if (inputs) {
+		for (let input of inputs) {
+			if (!input.value.length) {
+				input.style.border = "1px solid #8f1515"
 
-					// ИНФОРМАЦИЯ ДЛЯ БЭКЕНД-ПРОГРАММИСТА: ВОТ ТАК ВЫДЕЛЯЕТСЯ НЕВАЛИДНОЕ ПОЛЕ (В СЛУЧАЕ ЕСЛИ ЛОГИН/ПАРОЛЬ НЕ СОВПАЛИ ПРИ ЛОГИНЕ)
-					input.addEventListener("focus", () => {
-						input.style.border = "1px solid #424242"
-					})
+				// ИНФОРМАЦИЯ ДЛЯ БЭКЕНД-ПРОГРАММИСТА: ВОТ ТАК ВЫДЕЛЯЕТСЯ НЕВАЛИДНОЕ ПОЛЕ (В СЛУЧАЕ ЕСЛИ ЛОГИН/ПАРОЛЬ НЕ СОВПАЛИ ПРИ ЛОГИНЕ)
+				input.addEventListener("focus", () => {
+					input.style.border = "1px solid #424242"
+				})
 
-					input.addEventListener("focusout", () => {
-						input.style.border = "1px solid #cecfd2"
-					})
-				}
+				input.addEventListener("focusout", () => {
+					input.style.border = "1px solid #cecfd2"
+				})
 			}
-			const passwordInputs = document.querySelectorAll(
-				".form-data-password__input",
-			)
-			if (passwordInputs.length === 2) {
-				if (passwordInputs[0].value !== passwordInputs[1].value) {
-					const validationMessagePasswords = document.querySelector(
-						".validation-message-passwords",
-					)
-					if (validationMessagePasswords) {
-						validationMessagePasswords.style.display = "block"
-					}
-					for (let input of passwordInputs) {
-						if (input.value.length) {
-							input.addEventListener("focus", () => {
-								validationMessagePasswords.style.display = "none"
-							})
-						}
+		}
+		const passwordInputs = document.querySelectorAll(
+			".form-data-password__input",
+		)
+		if (passwordInputs.length === 2) {
+			if (passwordInputs[0].value !== passwordInputs[1].value) {
+				const validationMessagePasswords = document.querySelector(
+					".validation-message-passwords",
+				)
+				if (validationMessagePasswords) {
+					validationMessagePasswords.style.display = "block"
+				}
+				for (let input of passwordInputs) {
+					if (input.value.length) {
+						input.addEventListener("focus", () => {
+							validationMessagePasswords.style.display = "none"
+						})
 					}
 				}
 			}
 		}
-	})
-}
+	}
+})
+
+// Предпросмотр VVV
 
 const closePreviewButton = document.querySelector(".button-close-preview")
-
 const previewModal = document.querySelector(".preview")
-
 const previewButtonSectionCreativity = document.getElementById(
 	"previewButtonSectionCreativity",
 )
@@ -89,7 +91,6 @@ const previewButtonGalleryPhoto = document.getElementById(
 	"previewButtonGalleryPhoto",
 )
 const previewButtonHome = document.getElementById("previewButtonHome")
-
 const previewSectionCreativity = document.getElementById(
 	"previewSectionCreativity",
 )
@@ -101,47 +102,33 @@ const previewGalleryInterview = document.getElementById(
 const previewGalleryFilm = document.getElementById("previewGalleryFilm")
 const previewHome = document.getElementById("previewHome")
 
-if (previewButtonSectionCreativity) {
-	previewButtonSectionCreativity.addEventListener("click", function () {
-		previewSectionCreativity.style.display = "flex"
-	})
-}
+previewButtonSectionCreativity?.addEventListener("click", () => {
+	previewSectionCreativity.style.display = "flex"
+})
 
-if (previewButtonSectionWord) {
-	previewButtonSectionWord.addEventListener("click", function () {
-		previewSectionWord.style.display = "flex"
-	})
-}
+previewButtonSectionWord?.addEventListener("click", () => {
+	previewSectionWord.style.display = "flex"
+})
 
-if (previewButtonGalleryPhoto) {
-	previewButtonGalleryPhoto.addEventListener("click", function () {
-		previewGalleryPhoto.style.display = "flex"
-	})
-}
+previewButtonGalleryPhoto?.addEventListener("click", () => {
+	previewGalleryPhoto.style.display = "flex"
+})
 
-if (previewButtonGalleryInterview) {
-	previewButtonGalleryInterview.addEventListener("click", function () {
-		previewGalleryInterview.style.display = "flex"
-	})
-}
+previewButtonGalleryInterview?.addEventListener("click", () => {
+	previewGalleryInterview.style.display = "flex"
+})
 
-if (previewButtonGalleryFilm) {
-	previewButtonGalleryFilm.addEventListener("click", function () {
-		previewGalleryFilm.style.display = "flex"
-	})
-}
+previewButtonGalleryFilm?.addEventListener("click", () => {
+	previewGalleryFilm.style.display = "flex"
+})
 
-if (previewButtonHome) {
-	previewButtonHome.addEventListener("click", function () {
-		previewHome.style.display = "flex"
-	})
-}
+previewButtonHome?.addEventListener("click", () => {
+	previewHome.style.display = "flex"
+})
 
-if (closePreviewButton) {
-	closePreviewButton.addEventListener("click", function () {
-		previewModal.style.display = "none"
-	})
-}
+closePreviewButton?.addEventListener("click", () => {
+	previewModal.style.display = "none"
+})
 
 // Редактирование и сохранение изменений VVV
 
@@ -181,7 +168,7 @@ const initEditButtons = () => {
 			btn.addEventListener("click", onEditButtonClick)
 		})
 
-		if (saveAllButtons) {
+		if (saveAllButtons.length) {
 			saveAllButtons.forEach((btn) => {
 				btn.addEventListener("click", () => {
 					editButtons.forEach((btn) => {
@@ -209,9 +196,7 @@ const onLoad = () => {
 
 onLoad()
 
-// Загрузка файлов с прогресс-баром VVV
-
-// Вызов модалок
+// Вызов модалок VVV
 
 const modals = document.querySelectorAll(".modal")
 const buttonsCloseModal = document.querySelectorAll(".button-close-modal")
@@ -224,29 +209,66 @@ const modalFile = document.getElementById("modalFile")
 
 if (buttonDownloadPhotos) {
 	buttonDownloadPhotos.forEach((btn) => {
-		btn.addEventListener("click", function (e) {
+		btn.addEventListener("click", (e) => {
 			e.preventDefault()
 			modalPhoto.style.display = "flex"
 		})
 	})
 }
 
-if (buttonDownloadFiles) {
+if (buttonDownloadFiles.length) {
 	buttonDownloadFiles.forEach((btn) => {
-		btn.addEventListener("click", function (e) {
+		btn.addEventListener("click", (e) => {
 			e.preventDefault()
 			modalFile.style.display = "flex"
 		})
 	})
 }
 
-if (buttonsCloseModal) {
+if (buttonsCloseModal.length) {
 	buttonsCloseModal.forEach((btn) => {
-		btn.addEventListener("click", function (e) {
+		btn.addEventListener("click", (e) => {
 			e.preventDefault()
 			modals.forEach((modal) => {
 				modal.style.display = "none"
 			})
+		})
+	})
+}
+
+const photoUploaders = document.querySelectorAll('.upload-photo-wrapper > input')
+
+if(photoUploaders.length) {
+	photoUploaders.forEach(photoUploader => {
+		photoUploader?.addEventListener('change', (e) => {
+			const modalInformation = document.querySelector('.modal-information')
+			const modalLoading = document.querySelector('.modal-loading');
+			const modalUploaded = document.querySelector('.modal-uploaded');
+
+			const progress = document.querySelector('.modal-loading-progress-bar__filled')
+			const uploadedImage = document.querySelector('.modal-uploaded img');
+			const saveChangesButton = document.querySelector('.modal-uploaded__button');
+
+			const [photo] = photoUploader.files
+			if(photo) {
+				uploadedImage.src = URL.createObjectURL(photo)
+			}
+		
+			modalInformation.style.display = 'none'
+			modalLoading.style.display = 'flex'
+		
+			window.setTimeout(() => {
+				progress.classList.add('finished')
+			}, 800)
+		
+			window.setTimeout(() => {
+				modalLoading.style.display = 'none'
+				modalUploaded.style.display = 'flex'
+			}, 2000)
+
+			saveChangesButton.addEventListener('click', () => {
+				modalPhoto.style.display = 'none'
+			}) 
 		})
 	})
 }
